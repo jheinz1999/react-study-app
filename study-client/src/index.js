af63a6077429315d8eeab4d5f5be0e43ccd7b296
Jonathan Heinz <jonathan.heinz@aol.com>
2018-12-18 22:52:46 -0800

    set up redux

--- study-client/src/index.js
+++ study-client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import reducer from './redux/reducer';
+import './index.css';
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ study-client/src/redux/reducer.js
@@ -0,0 +1,18 @@
+// reducer.js - global reducer for redux store
+
+const initialState = {
+
+
+
+}
+
+export default (state = initialState, action) => {
+
+  switch (action.type) {
+
+    default:
+      return state;
+
+  }
+
+}
